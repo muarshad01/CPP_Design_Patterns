b6506582cfaa952990ec1115fee35cf9988e322f
Umer <41253434+muarshad01@users.noreply.github.com>
2024-04-28 21:42:40 -0400

    Update main.cc

--- Behavioral_Patterns/chain_of_responsibility/main.cc
+++ Behavioral_Patterns/chain_of_responsibility/main.cc
@@ -2,6 +2,8 @@
  * The Handler interface declares a method for building the chain of handlers.
  * It also declares a method for executing a request.
  */
+#include <iostream>
+
 class Handler
 {
 public:
